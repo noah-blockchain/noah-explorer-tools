98ba973e8907ca27d5541826ae6c16d00cdb5c10
Noah GO Developer <developer.chain.2019@hotmail.com>
2019-08-19 18:50:48 +0300

    fix dependencies and gitignore

--- models/stake.go
+++ models/stake.go
@@ -6,7 +6,7 @@ type Stake struct {
 	ValidatorID    uint64     `json:"validator_id"`
 	CoinID         uint64     `json:"coin_id"`
 	Value          string     `json:"value"     sql:"type:numeric(70)"`
-	BipValue       string     `json:"bip_value" sql:"type:numeric(70)"`
+	NoahValue      string     `json:"noah_value" sql:"type:numeric(70)"`
 	Coin           *Coin      `json:"coins"`                                  //Relation has one to Coins
 	OwnerAddress   *Address   `json:"owner_address" pg:"fk:owner_address_id"` //Relation has one to Addresses
 	Validator      *Validator `json:"validator"`                              //Relation has one to Validators
